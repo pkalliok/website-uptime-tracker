d480d575672779d7a00afbf69d80d295539b57ec
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2020-12-15 14:55:12 +0200

    Get rid of global mock_thread ('cause it wasn't needed)

--- test_mock_web_service.py
+++ test_mock_web_service.py
@@ -1,16 +1,15 @@
 
 import requests
 
-mock_thread = None
 service_url = 'http://localhost:5000/service'
 
 def setUpModule():
     from mock_web_service import mock
     from threading import Thread
     from time import sleep
-    mock_thread = Thread(target=mock.run)
-    mock_thread.daemon = True
-    mock_thread.start()
+    t = Thread(target=mock.run)
+    t.daemon = True
+    t.start()
     sleep(1) # simplest way to give the dev server time to start
 
 def test_default_response():
