3d80ce61fb6a2ab9d391e7ec743cf31831bd7ae2
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2020-12-17 21:49:32 +0200

    report_uptime() happy case passes; command line interface in uptime_producer

--- test_uptime_producer.py
+++ test_uptime_producer.py
@@ -1,10 +1,12 @@
 
 import requests
-from test_mock_web_service import run_mock_service_in_background, change_url
+from test_mock_web_service import run_mock_service_in_background, \
+        change_url, service_url
 from uptime_producer import report_uptime
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
+from json import loads
 
-kafka_cons = None
+kafka_cons, kafka_prod = None, None
 kafka_credentials = dict(
         bootstrap_servers=open('kafka.host').read(),
         security_protocol='SSL',
@@ -13,8 +15,9 @@ kafka_credentials = dict(
         ssl_certfile='kafka.cert'
 )
 
-def set_up_kafka_consumer():
-    global kafka_cons
+def set_up_kafka():
+    global kafka_cons, kafka_prod
+    kafka_prod = KafkaProducer(**kafka_credentials)
     kafka_cons = KafkaConsumer(
         "uptime",
         group_id='test-consumer',
@@ -24,11 +27,15 @@ def set_up_kafka_consumer():
 
 def setUpModule():
     run_mock_service_in_background()
-    set_up_kafka_consumer()
+    set_up_kafka()
 
 def test_working_site():
     assert len(kafka_cons.poll(timeout_ms=1000)) == 0
-    report_uptime()
+    report_uptime(service_url, lambda body: True, kafka_prod, 'uptime')
     messages = next(iter(kafka_cons.poll(timeout_ms=1000).values()))
     assert len(messages) == 1
+    msg = loads(messages[0].value.decode('utf-8'))
+    assert msg['httpStatus'] == 200
+    assert msg['passes'] == True
+    assert msg['delay'] < 1
 
--- uptime_producer.py
+++ uptime_producer.py
@@ -1,2 +1,59 @@
 
-def report_uptime(): pass
+import click, requests, sys, re
+from requests.exceptions import ConnectionError
+from time import sleep, perf_counter
+from kafka import KafkaProducer
+from json import dumps
+
+def make_request(url):
+    start = perf_counter()
+    try:
+        result = requests.get(url)
+        code = result.status_code
+        content = result.text
+    except ConnectionError:
+        code = 0
+        content = 'Connection refused'
+    end = perf_counter()
+    return ((end-start), code, content)
+
+def report_uptime(url, check, kafka, kafka_topic):
+    time, status, body = make_request(url)
+    message = dumps(dict(delay=time, httpStatus=status, passes=check(body)))
+    if kafka: kafka.send(kafka_topic, message.encode('utf-8'))
+    print('Check status:', status, 'in', time, 's')
+
+def kafka_connection(kafka_host, kafka_key_file,
+        kafka_cert_file, kafka_ca_file):
+    return KafkaProducer(
+            bootstrap_servers=kafka_host,
+            security_protocol='SSL',
+            ssl_cafile=kafka_ca_file,
+            ssl_keyfile=kafka_key_file,
+            ssl_certfile=kafka_cert_file
+    )
+
+@click.command()
+@click.argument('url')
+@click.option('--check-pattern', default=None)
+@click.option('--check-interval', type=int, show_default=True, default=60)
+@click.option('--kafka-host', default=None)
+@click.option('--kafka-key-file', default='kafka.key')
+@click.option('--kafka-cert-file', default='kafka.cert')
+@click.option('--kafka-ca-file', default='kafka.ca')
+@click.option('--kafka-topic', default='uptime')
+def loop(url, check_pattern, check_interval, kafka_topic, **kwargs):
+    kafka = kafka_connection(**kwargs) if kwargs['kafka_host'] else None
+    if check_pattern:
+        check_re = re.compile(check_pattern)
+        check = lambda body: bool(check_re.match(body))
+    else: check = lambda body: len(body)
+    while True:
+        try: report_uptime(url, check, kafka, kafka_topic)
+        except:
+            print('Got uncaught exception:')
+            print(sys.exc_info()[1])
+        sleep(check_interval)
+
+if __name__ == '__main__': loop()
+
