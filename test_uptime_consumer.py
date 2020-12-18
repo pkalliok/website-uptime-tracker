e491f01163e9279c34825e2b89387f32361c12c8
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2020-12-18 16:40:53 +0200

    First (failing) test for uptime consumer

--- test_uptime_consumer.py
+++ test_uptime_consumer.py
@@ -1,4 +1,5 @@
 
+from uptime_consumer import process_events, persist_event
 from test_uptime_producer import kafka_prod, set_up_kafka
 from json import dumps, loads
 import psycopg2
@@ -10,6 +11,26 @@ def set_up_postgres():
     if pg_conn: return
     pg_conn = psycopg2.connect(**loads(open("pg-creds.json").read()))
 
+def sql_query(query, args=None):
+    with pg_conn:
+        with pg_conn.cursor() as curs:
+            if args: curs.execute(query, args)
+            else:
+                curs.execute(query)
+                return curs.fetchall()
+
 def setUpModule():
-    #set_up_kafka()
+    set_up_kafka()
     set_up_postgres()
+
+def test_message_is_persisted():
+    ((max_id,),) = sql_query("select max(id) from uptime_events")
+    persist_event(dict(url="foo", httpStatus="200", body=""))
+    ((url, status, body),) = sql_query("""
+        select url, http_status, body
+        from uptime_events
+        where id > %s""", (max_id,))
+    assert url == 'foo'
+    assert status == 200
+    assert body == ''
+
---
+++ uptime_consumer.py
@@ -0,0 +1,3 @@
+
+def persist_event(): pass
+def process_events(): pass
